6558a2dd4ddea4c4388f001c63f50055b03e01ad
Jacob Martella <me@jacobmartella.com>
2022-06-10 21:25:05 -0500

    Added in tailwind css support for block development.

--- blocks/starter-block/build/index.asset.php
+++ blocks/starter-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1b8d15e97e28fba3f680');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f8245a46ad92a5e9d8ee');
